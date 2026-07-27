b21fa910760196ce54a11f5be46813758b745e4d
Hugo Carreras <hugo.carreras37@gmail.com>
2026-07-26 23:42:31 -0300

    Completo CRUD de negocios: se agrega el PUT y DELETE

--- negocios/views.py
+++ negocios/views.py
@@ -22,3 +22,28 @@ class NegocioDetalleView(APIView):
         if negocio is None:
             return Response({"error": "Negocio no encontrado"}, status=404)
         return Response(negocio)
+
+    def put(self, request, negocio_id):
+        nombre = request.data.get("nombre")
+
+        query = "UPDATE negocio SET nombre = %s WHERE id = %s"
+        parametros = [nombre, negocio_id]
+
+        execute_command(query, parametros)
+
+        filas_afectadas = execute_command.rowcount
+        if filas_afectadas == 0:
+            return Response({"error": "Negocio no encontrado"}, status=404)
+        return Response({"mensaje": "Negocio actualizado"})
+
+    def delete(self, request, negocio_id):
+
+        query = "DELETE FROM negocio WHERE id = %s"
+        parametros = [negocio_id]
+
+        execute_command(query, parametros)
+        
+        filas_afectadas = execute_command.rowcount
+        if filas_afectadas == 0:
+            return Response({"error": "Negocio no encontrado"}, status=404)
+        return Response({"mensaje": "Negocio eliminad"})
