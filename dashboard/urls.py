da2e14ea9ca0117870b00eee9569b3915bf357f5
Hugo Carreras <hugo.carreras37@gmail.com>
2026-08-10 17:27:25 -0300

    bug: layout link roto 3

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("evolucion-mensual/", views.EvolucionMensualView.as_view(), name="evolucion_mensual"),
     path("totales/", views.TotalesView.as_view(), name="totales"),
     path("productos-mas-vendidos/", views.ProductosMasVendidosView.as_view(), name="productos_mas_vendidos"),
+    path("proyeccion-vs-realidad/", views.ProyeccionVsRealidadView.as_view(), name="proyeccion_vs_realidad")
 ]
