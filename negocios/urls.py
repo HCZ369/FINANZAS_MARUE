6710b76a092c83f44da441ffca5b8d0c6e4399dd
Hugo Carreras <hugo.carreras37@gmail.com>
2026-08-12 09:39:37 -0300

    Elimino endpoint con acceso a base de datos

--- negocios/urls.py
+++ negocios/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.NegociosView.as_view(), name="negocios"),
-    path("setup-tablas/", views.SetupTablasView.as_view(), name="setup_tablas"),
     path("<int:negocio_id>/", views.NegocioDetalleView.as_view(), name="negocio_detalle"),
 ]
