91707c2a073045ff1c4b2149f846768d7b08cfde
Hugo Carreras <hugo.carreras37@gmail.com>
2026-08-12 09:05:33 -0300

    bug: negociosurl corregido

--- negocios/urls.py
+++ negocios/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.NegociosView.as_view(), name="negocios"),
+    path("setup-tablas/", views.SetupTablasView.as_view(), name="setup_tablas"),
     path("<int:negocio_id>/", views.NegocioDetalleView.as_view(), name="negocio_detalle"),
 ]
